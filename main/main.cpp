08afffb21c4ae3cd2f00891f72f05dd95e9ae120
bytebangshu <mail.debangshupramanik@gmail.com>
2026-04-02 20:19:51 -0500

    Last commit before I diverge from main again

--- main/main.cpp
+++ main/main.cpp
@@ -105,6 +105,7 @@ extern "C" void app_main(void) {
 	ESP_LOGI("INFO", "it worked out!");
 
     // Just launch the task and let it run
+		
     
     // app_main can now just chill or handle other things (like WiFi/HTTP)
     while(1) { vTaskDelay(pdMS_TO_TICKS(1000)); }
