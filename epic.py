a99df10374355fe490169d36e1a5f89d23cc00e2
bytebangshu <mail.debangshupramanik@gmail.com>
2026-04-12 08:29:56 -0500

    delete testing files

--- epic.py
+++
@@ -1,8 +0,0 @@
-import flatbuffers
-import tensorflow as tf
-
-interpreter = tf.lite.Interpreter("tinyvgg_terrain_quantized_int8_w_head.tflite")
-interpreter.allocate_tensors()
-
-for detail in interpreter.get_tensor_details():
-    print(detail['index'], detail['name'], detail['shape'], detail['dtype'])
